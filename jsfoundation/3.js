9a9c7e7ef9bea1b7c7a92e0af94c2073400a9a54
rifki abrar <rifki.abrar11@gmail.com>
2020-02-22 14:17:29 +0700

    latihan 3 selesai

--- jsfoundation/3.js
+++ jsfoundation/3.js
@@ -5,8 +5,13 @@
  * Output: The largest number
  */
 
-const solution = () => {
-  // Write the solution here
+const solution = (num1, num2, num3) => {
+  if ((num3 > num2) && (num3 > num1)){
+    return num3
+  } else if ((num1 > num2) && (num1 > num3)){
+    return num1
+  }
+  {return num2}
 };
 
 module.exports = {
